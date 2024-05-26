d4db993d4fa553ba39dd55c5544814cb6edd3518
C22410612 <C22410612@mytudublin.ie>
2024-05-26 16:14:37 +0100

    Added a followUp ping command - Tests followUps

---
+++ commands/utility/pingFollowUp.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports =
+{
+    data: new SlashCommandBuilder()
+        .setName('followup_ping')
+        .setDescription('Replies with ping and then sends a secret message!'),
+
+    async execute(interaction)
+    {
+        await interaction.reply('Pong!');
+        await wait(5_000);
+        await interaction.followUp({ content: 'Not really! :face_with_hand_over_mouth:', ephemeral: true });
+    },
+};
