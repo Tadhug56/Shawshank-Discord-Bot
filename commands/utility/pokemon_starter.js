081e2ccb1c3b23bbc00f055ef4bbf5a74f0a504d
C22410612 <C22410612@mytudublin.ie>
2024-05-30 23:48:59 +0100

    Added a pokemon starter command - Tests select menus

---
+++ commands/utility/pokemon_starter.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, ActionRowBuilder} = require('discord.js');
+
+module.exports =
+{
+    data: new SlashCommandBuilder()
+        .setName('pokemon_starter')
+        .setDescription('Select a pokemon starter'),
+
+    async execute(interaction)
+    {
+        const select = new StringSelectMenuBuilder()
+            .setCustomId('starter')
+            .setPlaceholder('Make a selection!')
+            .addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Bulbasaur')
+                    .setDescription('The dual-type Grass/Poison Seed Pokémon.')
+                    .setValue('bulbasaur'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Charmander')
+                    .setDescription('The Fire-type Lizard Pokémon.')
+                    .setValue('charmander'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Squirtle')
+                    .setDescription('The Water-type Tiny Turtle Pokémon.')
+                    .setValue('squirtle'),
+            );
+
+        const row = new ActionRowBuilder()
+                .addComponents(select);
+        
+        await interaction.reply(
+        {  
+            content: 'Choose your own starter!',
+            components: [row],
+        });
+    },
+};
