9a95b5932a6e451d80748f49a5668ab945c37d3a
C22410612 <C22410612@mytudublin.ie>
2024-07-09 19:36:24 +0100

    Update Command : league - Added functionality to get a match history of a specific player - Added functionality to get specific match data of from a player's history - Added functionality to get the players details from that match

--- commands/shawshank/league.mjs
+++ commands/shawshank/league.mjs
@@ -1,13 +1,36 @@
 import fetch from 'node-fetch';
 
-const riotKey = 'RGAPI-35ff0fc4-6f80-41ec-b83f-474c7e6bf4a4';
+const riotKey = 'RGAPI-a0454f01-b90d-4b55-a0ac-5c184940f86c';
 
 const tagline = 'IRE';
 const gameName = 'Tadhug56';
 
 const region = 'europe';
 
-const getAccountByRiotId = async () =>
+// Main function
+
+async function main()
+{
+    const puuid = await getAccountByRiotId();
+    if (puuid) 
+    {
+        const matchList = await getMatchList(puuid);
+
+        if (matchList)
+        {
+            const matchArray = matchList
+
+            if (matchArray && matchArray.length > 0)
+            {
+                const matchData = await getMatchData(matchArray[0]);
+
+                getParticipantData(matchData, puuid);
+            }
+        }
+    }
+}
+
+async function getAccountByRiotId()
 {
     try
     {
@@ -19,6 +42,8 @@ const getAccountByRiotId = async () =>
 
         const data = await response.json();
         console.log(data);
+
+        return data.puuid;
     }
     
     catch (error)
@@ -27,4 +52,79 @@ const getAccountByRiotId = async () =>
     }
 };
 
-getAccountByRiotId();
+async function getMatchList(puuid)
+{
+    try
+    {
+        const response = await fetch(`https://${region}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${riotKey}`)
+
+        if (!response.ok)
+        {
+            throw new Error(`HTTP error! Status : ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data;
+    }
+
+    catch(error)
+    {
+        console.error('Error:', error.message);
+    }
+};
+
+async function getMatchData(matchId)
+{
+    try
+    {
+        const response = await fetch(`https://${region}.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${riotKey}`);
+
+        if (!response.ok)
+        {
+            throw new Error(`HTTP error! Status : ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data;
+    }
+
+    catch(error)
+    {
+        console.error('Error:', error.message);
+    }
+};
+
+async function getParticipantData(matchData, targetPuuid)
+{
+    try
+    {
+        const participants = matchData.info.participants;
+        let targetParticipant;
+
+        participants.forEach(participant =>
+        {
+            console.log('Puuid : ', participant.puuid);
+            if (participant.puuid === targetPuuid)
+            {
+                targetParticipant = participant;
+            }
+        });
+
+        console.log(targetParticipant);
+
+    }
+
+    catch(error)
+    {
+        console.error('Error:', error.message);
+    }
+    
+};
+
+main();
