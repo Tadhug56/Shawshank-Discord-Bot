56f2ca164357184e5d0f2568ec60075e9cebef57
C22410612 <C22410612@mytudublin.ie>
2024-07-15 23:24:45 +0100

    Update Command : league_matches - The buttons to toggle betwene matches now disable based on if the action is possible - Added some new comments to explain parts of the script

--- commands/shawshank/league_matches.js
+++ commands/shawshank/league_matches.js
@@ -18,11 +18,15 @@ module.exports =
 
     async execute(interaction)
     {
-
+        // Match count variables
         let count = 0;
+        const maxCount = 10;
+
+        // Riot ID variables
         const username = interaction.options.getString('username');
         const tagline = interaction.options.getString('tagline');
 
+        // Calls the backend for the stats and displays them in an embed
         const handleInteraction = async () =>
         {
             let stats = await getStats(username, tagline, count, interaction);
@@ -54,12 +58,14 @@ module.exports =
                 new ButtonBuilder()
                     .setCustomId('previous')
                     .setLabel('Previous')
-                    .setStyle(ButtonStyle.Secondary),
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(count === 0),
 
                 new ButtonBuilder()
                     .setCustomId('next')
                     .setLabel('Next')
                     .setStyle(ButtonStyle.Primary)
+                    .setDisabled(count === maxCount)
             );
 
             await interaction.reply({ embeds: [embed], components: [row] });
@@ -96,27 +102,44 @@ module.exports =
                 }
 
                 const newEmbed = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle(`${username} OP.GG`)
-                .setURL(`https://www.op.gg/summoners/euw/${username}-${tagline}`)
-                .setDescription('Your League stats from recent matches')
-                .setThumbnail(stats.imageUrl)
-                .addFields(
-                    { name: 'Name', value: `${stats.name}`, inline: true},
-                    //{ name: '\u200B', value: '\u200B' }
-                )
-                .addFields(
-                    { name: 'Champion', value: `${stats.championName}`, inline: true },
-                    { name: ' ', value: ' ', inline: true }
-                )
-                .setImage(stats.championImage)
-                .addFields(
-                    { name: 'Kills', value: `${stats.kills}`, inline: true },
-                    { name: 'Deaths', value: `${stats.deaths}`, inline: true },
-                    { name: 'Assists', value: `${stats.assists}`, inline: true },
-                );
-                
-                await i.editReply({ embeds: [newEmbed], components: [row] });
+                    .setColor(0x0099FF)
+                    .setTitle(`${username} OP.GG`)
+                    .setURL(`https://www.op.gg/summoners/euw/${username}-${tagline}`)
+                    .setDescription('Your League stats from recent matches')
+                    .setThumbnail(stats.imageUrl)
+                    .addFields(
+                        { name: 'Name', value: `${stats.name}`, inline: true},
+                        //{ name: '\u200B', value: '\u200B' }
+                    )
+                    .addFields(
+                        { name: 'Champion', value: `${stats.championName}`, inline: true },
+                        { name: ' ', value: ' ', inline: true }
+                    )
+                    .setImage(stats.championImage)
+                    .addFields(
+                        { name: 'Kills', value: `${stats.kills}`, inline: true },
+                        { name: 'Deaths', value: `${stats.deaths}`, inline: true },
+                        { name: 'Assists', value: `${stats.assists}`, inline: true },
+                    );
+
+                // New button
+
+                const newRow = new ActionRowBuilder().addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('previous')
+                        .setLabel('Previous')
+                        .setStyle(ButtonStyle.Secondary)
+                        .setDisabled(count === 0),
+
+                    new ButtonBuilder()
+                        .setCustomId('next')
+                        .setLabel('Next')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(count === maxCount)
+                    );
+
+
+                await i.editReply({ embeds: [newEmbed], components: [newRow] });
             });
 
             collector.on('end', collected =>
@@ -130,18 +153,20 @@ module.exports =
     }
 };
 
+// Takes in users Riot ID information and gives it to the API call functions
 async function getStats(uName, tLine, count, interaction)
 {
     const { fetchMatchData } = await import('./league.mjs');
     const matchStats = await fetchMatchData(count, uName, tLine);
-    
-    
+
+    // If the stats weren't retrieved correctly send an error
     if(!matchStats)
     {
         await interaction.reply({ content: 'Failed to fetch match data. Please try again later.', ephemeral: true });
         return;
     }
 
+    // Else return the stats
     else
     {
         return matchStats;
